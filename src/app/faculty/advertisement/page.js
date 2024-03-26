e645a5d430775bc743ab860cf1c4b8576d8242df
Parth Naik <parthn254@gmail.com>
2024-03-26 15:05:56 +0530

    Advertisement page

--- src/app/faculty/advertisement/page.js
+++ src/app/faculty/advertisement/page.js
@@ -1,12 +1,12 @@
+import bits_logo from '../../../assets/bits_logo.png';
+import Image from 'next/image';
 export default function Advertisement() {
     return(
       <main>
-        <div>
-          <p>
-            THIS IS ADVERTISEMENT PAGE
-          </p>
-        </div>
-      </main>
+      <div className="flex flex-col items-center bg-sky-100 h-screen justify-center">
+        <Image src={bits_logo} className="justify-center items-center ml-12 mt-4 md:h-[15%] md:w-[30%]" alt="BITS Logo" />
+      </div>
+    </main>
       
     );
 }
