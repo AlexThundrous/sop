8e81aeaa0c2db3550a45984329f17458859e1cd8
Saurav <alexthundrous2104@gmail.com>
2024-03-26 15:29:37 +0530

    UI fixes

--- src/app/faculty/advertisement/page.js
+++ src/app/faculty/advertisement/page.js
@@ -3,8 +3,8 @@ import Image from 'next/image';
 export default function Advertisement() {
     return(
       <main>
-      <div className="flex flex-col items-center bg-sky-100 h-screen justify-center">
-        <Image src={bits_logo} className="justify-center items-center ml-12 mt-4 md:h-[15%] md:w-[30%]" alt="BITS Logo" />
+      <div className="flex flex-col bg-sky-100 h-screen justify-start">
+        <Image src={bits_logo} className="justify-start items-start ml-12 mt-4 md:h-[15%] md:w-[30%]" alt="BITS Logo" />
       </div>
     </main>
       
