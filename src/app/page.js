34f264288c350ae475911e9c55a2757e9c27a543
Saurav <alexthundrous2104@gmail.com>
2024-03-27 16:35:52 +0530

    Loading screen

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,35 @@
 'use client';
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
-import React,{ useState } from 'react';
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 
 export default function Home() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+
+      router.push('/faculty');
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+
   return (
-    <main className="bg-blue-600 flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="bg-blue-600 flex min-h-screen items-center justify-center">
+      <div className="flex gap-2">
+      <span className='sr-only'>Loading...</span>
+        <div className="w-5 h-5 rounded-full  bg-black animate-bounce [animation-delay:-0.3s]"></div>
+        <div className="w-5 h-5 rounded-full  bg-black animate-bounce [animation-delay:-0.15s]"></div>
+        <div className="w-5 h-5 rounded-full animate-bounce bg-black"></div>
+      </div>
+      {isLoading && <></>}
     </main>
   );
 }
